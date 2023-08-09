85874a01d7754bd0cf0e1d214659542986daff25
Kent Nguyen <kentn@Kents-MacBook-Air.local>
2023-08-08 20:25:31 -0500

    fetched search from monster.com using query string insertion and userAgent

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,7 +1,39 @@
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+import java.io.IOException;
+
 public class Scraper {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Document doc;
+        try {
+            // fetching the target website returns a Connection object
+            doc = Jsoup.connect("https://www.monster.com/jobs/search")
+                    .data("q","software engineer intern")
+                    .data("where","")
+                    .data("page","1")
+                    .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
+                    .get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        //now we have a document object to work with
+        System.out.println(doc.location());
+        System.out.println(doc.title());
+//        System.out.println(doc.text());
 
-
+//        try{
+//
+//            String strText =
+//                    Jsoup
+//                            .connect("https://www.google.com/")
+//                            .get()
+//                            .text();
+//
+//            System.out.println(strText);
+//
+//        }catch(IOException ioe){
+//            System.out.println("Exception: " + ioe);
+//        }
     }
 }
