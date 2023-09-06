7169b37432f85d2ea26e8bb753efaa723eabdcdc
Kent Nguyen <kentn@kents-air.gateway.sonic.net>
2023-09-05 18:55:04 -0700

    Tested grabbing href from all <a> (anchor) tags

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,5 +1,6 @@
 import org.jsoup.*;
-import org.jsoup.nodes.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.*;
 import java.io.IOException;
 
@@ -8,32 +9,42 @@ public class Scraper {
         Document doc;
         try {
             // fetching the target website returns a Connection object
-            doc = Jsoup.connect("https://www.monster.com/jobs/search")
-                    .data("q","software engineer intern")
-                    .data("where","")
-                    .data("page","1")
+//            doc = Jsoup.connect("https://www.monster.com/jobs/search")
+            doc = Jsoup.connect("https://www.monster.com/jobs/search?q=software+engineer&where=&page=1")
+//                    .data("q","software engineer intern")
+//                    .data("where","")
+//                    .data("page","1")
                     .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
                     .get();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        //now we have a document object to work with
+
+        //test print to check current url location and title
         System.out.println(doc.location());
         System.out.println(doc.title());
-//        System.out.println(doc.text());
 
-//        try{
-//
-//            String strText =
-//                    Jsoup
-//                            .connect("https://www.google.com/")
-//                            .get()
-//                            .text();
-//
-//            System.out.println(strText);
-//
-//        }catch(IOException ioe){
-//            System.out.println("Exception: " + ioe);
+//        Element links  = doc.getElementsByClass("sc-gAjuZT cUPTNR").first();
+//        for (Element link : links){
+//            String linkHref = link.attr("href");
+//            String linkText = link.text();
+//            System.out.println(linkHref);
 //        }
+
+
+//        System.out.println(doc);
+
+        /*
+        a test to collect all <a> tags and any href attribute associated with each tag
+        */
+
+//        Elements jobTitleList = doc.getElementsByClass("sc-gAjuZT cUPTNR"); //returns an ArrayList<Element>
+        Elements jobTitleList = doc.getElementsByTag("a");
+        System.out.println(jobTitleList.size());
+        for (Element element : jobTitleList){
+            System.out.println(element.attr("href"));
+        }
+
+
     }
 }
