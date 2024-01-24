4635982b1ea4cd62f63e65c3a08faa83618f4f63
Kent Nguyen <kentn@kents-air.gateway.sonic.net>
2024-01-23 20:03:23 -0800

    Refactored Importer class to ImportToMongoDB

--- src/main/java/ImportToMongoDB.java
+++ src/main/java/ImportToMongoDB.java
@@ -14,7 +14,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-public class Importer {
+public class ImportToMongoDB {
 
     public static void main(String[] args) {
 
