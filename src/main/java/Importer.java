519f087f92299819df80426945c4e2b998169b0b
Kent Nguyen <kentn@kents-air.gateway.sonic.net>
2024-01-23 19:23:27 -0800

    Encapsulated mongoDB connection

--- src/main/java/Importer.java
+++ src/main/java/Importer.java
@@ -10,6 +10,7 @@ import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
@@ -18,40 +19,23 @@ public class Importer {
     public static void main(String[] args) {
 
         String filename = "src/main/DataCSV.txt";
-        //create mongoDB connection
+
+        //create mongoDB connection string
         String connectionString = "mongodb+srv://nguyenkkent:QS1UKls5SSiQGJbE@jobpostingcluster.zr8ofhr.mongodb.net/?retryWrites=true&w=majority";
+        MongoCollection<Document> collection = connectToMongoDB(connectionString);
 
-        ServerApi serverApi = ServerApi.builder()
-                .version(ServerApiVersion.V1)
-                .build();
+        if (collection!=null){
+            //parse CSV file
+            ArrayList<String> listOfLines = parseCSVFile(filename);
 
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(new ConnectionString(connectionString))
-                .serverApi(serverApi)
-                .build();
-
-        // Create a new client and connect to the server
-        try (MongoClient mongoClient = MongoClients.create(settings)) {
-            try {
-                // Send a ping to confirm a successful connection
-                MongoDatabase database = mongoClient.getDatabase("admin");
-                database.runCommand(new Document("ping", 1));
-                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-            } catch (MongoException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-        //parse CSV file
-        ArrayList<String> listOfLines = parseCSVFile(filename);
-
-        //Create document and insert into database
+            //Create document and insert into database
 //        for (String str : listOfLines){
 //            insertToMongo
 //        }
-
-
+        }
+        else{
+            System.out.println("Unable to connect to Database");
+        }
 
     }//end of main
 
@@ -82,7 +66,37 @@ public class Importer {
         return listOfLines;
     }
 
+    /**
+     *  Function: connectToMongoDB
+     *  Creates a connection to MongoDB Atlas Cloud Database
+     *  @param connectionString
+     *  Returns MongoCollection<Document> if successful
+     **/
+    private static MongoCollection<Document> connectToMongoDB(String connectionString){
 
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+//                MongoDatabase database = mongoClient.getDatabase("admin");
+//                database.runCommand(new Document("ping", 1));
+//                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+                MongoDatabase database = mongoClient.getDatabase("WebScrapper");
+                 return database.getCollection("JobPostings");
+            } catch (MongoException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
 
 }
 
