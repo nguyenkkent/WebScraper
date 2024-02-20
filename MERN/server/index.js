b1ee0c0110e2c4b5dc7dffc0c657a231e1c85d4c
Kent Nguyen <kentn@kents-air.gateway.sonic.net>
2024-02-19 21:49:22 -0800

    refactored index.js and created about me route

--- MERN/server/index.js
+++ MERN/server/index.js
@@ -4,15 +4,17 @@ import mongoose from "mongoose";
 
 const app = express();
 
-// app.listen(PORT, () => {
-//     console.log(`App is listening on port: ${PORT}`);//use backquotes for template literals
-// });
-
+//route for home page
 app.get('/', (request, response)=>{
     //console.log(request);
-    return response.status(234).send('Home page of our app');
+    return response.status(200).send('Home page of our app');
 });
 
+app.get('/about', (request, response)=>{
+    return response.status(200).send("Reserved section for \"about me\"");
+})
+
+//connect to mongoDB
 mongoose
     .connect(mongoDBURI)
     .then(()=>{
