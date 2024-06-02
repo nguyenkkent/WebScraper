62b130fc9dfdaca509d7fc9e1d078824158e9f03
nguyenkkent <nguyen.k.kent@gmail.com>
2024-06-02 11:35:26 +0700

    Completed parseJson function

--- server/server.js
+++ server/server.js
@@ -10,26 +10,28 @@ const app = express();
 
 //route for home page
 app.get('/', (request, response)=>{
-    //console.log(request);
-    return response.status(200).send('Home page of our app');
+  //console.log(request);
+  return response.status(200).send('Home page of our app');
 });
 
 app.get('/about', (request, response)=>{
-    return response.status(200).send("Reserved section for \"about me\"");
+  return response.status(200).send("Reserved section for \"about me\"");
 })
 
-//provide relatie path to text file with json objects
-parseJson("../src/main/DataJson.txt");
+
 
 //connect to mongoDB
-// mongoose
-//     .connect(process.env.mongoURL)
-//     .then(()=>{
-//         console.log('App successfully connected to database');
-//         app.listen(process.env.PORT, () => {
-//             console.log(`App is listening on port: ${process.env.PORT}`);
-//         });
-//     })
-//     .catch((error)=>{
-//         console.log(error);
-//     });
+mongoose
+  .connect(process.env.mongoURL)
+  .then(()=>{
+      console.log('App successfully connected to database');
+      app.listen(process.env.PORT, () => {
+          console.log(`App is listening on port: ${process.env.PORT}`);
+      });
+  })
+  .catch((error)=>{
+      console.log(error);
+  });
+  
+//provide relatie path to text file with json objects
+//parseJson("../src/main/DataJson.txt");
