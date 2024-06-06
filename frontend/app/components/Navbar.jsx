eb23a09078c2125f836fbbcc995b4eb65848cbba
nguyenkkent <nguyen.k.kent@gmail.com>
2024-06-06 14:23:41 +0700

    Added basic template Navbar component

---
+++ frontend/app/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Placeholder</h1>
+      <Link href="/">Dashboard</Link>
+    </nav>
+  )
+}
---
+++ frontend/app/page.jsx
@@ -0,0 +1,4 @@
+//this is the default ./ root page
+export default function Page() {
+  return <h1>This is the root/dashboard component</h1>
+}
